645c2a5a6f6fba4882a852d263e18a2806efd829
Molksy <mikefromenclave@gmail.com>
2023-01-31 11:59:59 +0200

    add js-core and images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import './js/slider-con';
 import './js/imask-set';
 import './js/select-set';
 import './js/modals-set';
+import './js/slider-details';
+import './js/slider-hero';
+import './js/slider-main';
