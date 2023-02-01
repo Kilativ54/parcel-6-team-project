10c06ff0830737643eda8532cc153d659d2e0854
Kilativ54 <113536851+Kilativ54@users.noreply.github.com>
2023-02-01 12:16:30 +0000

    Deploying to gh-pages from @ Kilativ54/parcel-6-team-project@d350ebd6fc9f33664be8c20570b74c95ea480c67 🚀

---
+++ number.c62210f2.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,loopedSlide:3,freeMode:!1,initialSlide:0,slidesPerView:1,navigation:{nextEl:".hero-room__swiper-button-next",prevEl:".hero-room__swiper-button-prev"}});
+//# sourceMappingURL=number.c62210f2.js.map
