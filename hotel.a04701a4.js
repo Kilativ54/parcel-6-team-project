68afdd729906a8796c39cc9656ce868047a32646
Kilativ54 <113536851+Kilativ54@users.noreply.github.com>
2023-02-03 07:58:38 +0000

    Deploying to gh-pages from @ Kilativ54/parcel-6-team-project@a121f773521a85b7e24a537436d09d89e446c402 🚀

--- hotel.a04701a4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-popup-target]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=hotel.a04701a4.js.map
